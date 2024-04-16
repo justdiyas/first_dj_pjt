0e84562d087bf34d3098b5fd5f16bbd52022d503
Diyas Iyemberdiyev <iyemberdiyev@gmail.com>
2024-04-16 12:54:07 +0500

    Added user forms, template and created register view mapping it to url

--- justblog/user/views.py
+++ justblog/user/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserRegisterForm
+from django.contrib import messages
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        forms = UserRegisterForm(request.POST)
+        if forms.is_valid():
+            forms.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('blog:blog-main')
+        else:
+            forms = UserRegisterForm()
+    return render(request, 'user/register.html', {'forms': forms})
