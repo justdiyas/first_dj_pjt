e10dbfa77a334e320f35c8f62c7e105e2693cd38
Diyas Iyemberdiyev <iyemberdiyev@gmail.com>
2024-04-16 14:44:56 +0500

    Made a few styling changes to register.html template

--- justblog/user/views.py
+++ justblog/user/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render, redirect
 from .forms import UserRegisterForm
 from django.contrib import messages
 
+
 def register(request):
     if request.method == 'POST':
-        forms = UserRegisterForm(request.POST)
-        if forms.is_valid():
-            forms.save()
-            messages.success(request, 'Your account has been successfully created!')
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account has been successfully created for {username}!')
             return redirect('blog:blog-main')
-        else:
-            forms = UserRegisterForm()
-    return render(request, 'user/register.html', {'forms': forms})
+    else:
+        form = UserRegisterForm()
+    return render(request, 'user/register.html', {'form': form})
