e624dff302fc8488074e4b4f0e14f3fbabd45f4e
Diyas Iyemberdiyev <iyemberdiyev@gmail.com>
2024-04-16 12:30:16 +0500

    Created user app

--- justblog/justblog/settings.py
+++ justblog/justblog/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.admin',
     'blog.apps.BlogConfig',
+    'user.apps.UserConfig',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
