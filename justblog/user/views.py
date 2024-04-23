c52be216a9983c190d0defacec996b008c97f003
Diyas Iyemberdiyev <iyemberdiyev@gmail.com>
2024-04-23 16:26:58 +0500

    Added user and profile forms, modified views accordingly in order to be able to update profile information including image

--- justblog/user/views.py
+++ justblog/user/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib import messages
 from django.contrib.auth import views, logout
 from django.contrib.auth.decorators import login_required
@@ -27,4 +27,20 @@ def logout_view(request):
 
 @login_required
 def profile(request):
-    return render(request, 'user/profile.html')
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Your profile has been updated!')
+            return redirect('user:user-profile') 
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'user_form': user_form,
+        'profile_form': profile_form,
+    }
+    return render(request, 'user/profile.html', context)
