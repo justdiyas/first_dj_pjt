44432d5f2306a88cfd96d4d9f7db542f2e787e61
Diyas Iyemberdiyev <iyemberdiyev@gmail.com>
2024-04-25 14:25:44 +0500

    Added new polls_detail.html file. Populated it with the form that displays available polls choices to vote. Created views and mapped it to url

--- justblog/polls/urls.py
+++ justblog/polls/urls.py
@@ -5,4 +5,6 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     path('', views.IndexListView.as_view(), name='polls-index'),
+    path('<int:pk>/detail/', views.IndexDetailView.as_view(), name='polls-detail'),
+    path('<int:pk>/vote/', views.vote, name='polls-vote'),
 ]
--- justblog/polls/views.py
+++ justblog/polls/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
 from .models import Question
 from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
 
 class IndexListView(generic.ListView):
-    template_name = 'polls/index.html'
+    template_name = 'polls/polls_index.html'
     queryset = Question.objects.all()
 
 
+class IndexDetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/polls_detail.html'
+
+def vote(request, pk):
+    question = get_object_or_404(Question, pk=pk)
+    messages.success(request, f'Your vote is counted')
+    return HttpResponseRedirect(reverse('polls:polls-detail', args=(question.id,)))
