6540658eb40247d5700aca33cfd3efacd426682b
Diyas Iyemberdiyev <iyemberdiyev@gmail.com>
2024-04-26 11:32:29 +0500

    Added a new was_published_recently method to Question model class to check if question was published within the last week

--- justblog/polls/models.py
+++ justblog/polls/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
-
+import datetime
 
 class Question(models.Model):
     question_text = models.CharField(max_length=255)
     publication_date = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def was_published_recently(self):
+        week_ago = timezone.now() - datetime.timedelta(days=7)
+        return week_ago < self.publication_date < timezone.now()
 
     def __repr__(self):
         return f'Author: {self.author} \nQuestion: {self.question_text}'
