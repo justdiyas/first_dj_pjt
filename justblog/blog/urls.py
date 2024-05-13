318ea10c08965834abe19564c99d27344843cfa3
Diyas Iyemberdiyev <iyemberdiyev@gmail.com>
2024-05-13 17:45:31 +0500

    Updated urlpatterns of all apps

--- justblog/blog/urls.py
+++ justblog/blog/urls.py
@@ -4,10 +4,10 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     path('', views.HomeView.as_view(), name='blog-home'),
-    path('blog/', views.PostListView.as_view(), name='blog-main'),
-    path('user/<str:username>', views.UserPostListView.as_view(), name='user-blog'),
-    path('blog/post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
-    path('blog/post/new/', views.PostCreateView.as_view(), name='post-create'),
-    path('blog/post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
-    path('blog/post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
+    path('post/', views.PostListView.as_view(), name='blog-main'),
+    path('post/user/<str:username>', views.UserPostListView.as_view(), name='user-blog'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', views.PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
 ]
--- justblog/justblog/urls.py
+++ justblog/justblog/urls.py
@@ -22,14 +22,14 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', include('blog.urls')),
-    path('register/', include('user.urls')),
+    path('blog/', include('blog.urls')),
+    path('user/', include('user.urls')),
     path('polls/', include('polls.urls')),
-    path('register/password-reset/',
+    path('user/password-reset/',
          auth_views.PasswordResetView.as_view(template_name='user/password_reset.html'), name='password_reset'),
-    path('register/password-reset/done/',
+    path('user/password-reset/done/',
          auth_views.PasswordResetDoneView.as_view(template_name='user/password_reset_done.html'), name='password_reset_done'),
-    path('register/password-reset-confirm/<uidb64>/<token>/',
+    path('user/password-reset-confirm/<uidb64>/<token>/',
          auth_views.PasswordResetConfirmView.as_view(template_name='user/password_reset_confirm.html'), name='password_reset_confirm'),
 ]
 
