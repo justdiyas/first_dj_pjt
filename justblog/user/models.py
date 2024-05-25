89462c0686942ad84ac3ab4154c5e04883fccbcd
Diyas Iyemberdiyev <iyemberdiyev@gmail.com>
2024-05-25 12:18:54 +0500

    Amended save method in user/models.py file

--- justblog/user/models.py
+++ justblog/user/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
@@ -10,10 +11,9 @@ class Profile(models.Model):
         return f'{self.user.username} profile'
 
     def save(self, *args, **kwargs):
-        super().save(*args)
+        super().save(*args, **kwargs)
         img = Image.open(self.image.path)
         if img.height > 125 or img.width > 125:
             output_size = (125, 125)
             img.thumbnail(output_size)
             img.save(self.image.path)
-
