d234b71ee14f04c125d22b0898a7454973738db1
Diyas Iyemberdiyev <iyemberdiyev@gmail.com>
2024-05-30 11:43:48 +0500

    Created Gallery model and overwritten save method

--- justblog/gallery/models.py
+++ justblog/gallery/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
 
-# Create your models here.
+
+class Gallery(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='gallery_pics')
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        img = Image.open(self.image.path)
+        if img.width > 400 and img.height > 600:
+            output_size = (400, 600)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
